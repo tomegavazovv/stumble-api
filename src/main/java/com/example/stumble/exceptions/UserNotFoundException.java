c5ddc05eb489465fc0edd83994e92cc295ab4749
tomegavazovv <tomegavazov@gmail.com>
2023-02-22 12:25:11 +0100

    first commit

--- src/main/java/com/example/stumble/exceptions/UserNotFoundException.java
+++ src/main/java/com/example/stumble/exceptions/UserNotFoundException.java
@@ -1,8 +1,4 @@
 package com.example.stumble.exceptions;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
 public class UserNotFoundException extends RuntimeException{
 }
