d6ed427a0c364804ff478a025cbf6351dd70289d
tomegavazovv <tomegavazov@gmail.com>
2023-02-22 16:35:14 +0100

    data.sql

---
+++ data.sql
@@ -0,0 +1,23 @@
+INSERT INTO users (id, first_name, last_name, email, description, gender, instagram_account, facebook_account, linkedin_account, image, lat, lon)
+VALUES (1, 'John', 'Doe', 'johndoe@gmail.com', 'Software Engineer', 'MALE', 'johndoe_insta', 'johndoe_fb', 'johndoe_linkedin', 'photo1.jpeg', 37.7749, -122.4194);
+
+INSERT INTO users (id, first_name, last_name, email, description, gender, instagram_account, facebook_account, linkedin_account, image, lat, lon)
+VALUES (2, 'Jane', 'Doe', 'janedoe@gmail.com', 'Doctor', 'FEMALE', 'janedoe_insta', 'janedoe_fb', 'janedoe_linkedin', 'photo2.jpeg', 37.7749, -122.4194);
+
+INSERT INTO users (id, first_name, last_name, email, description, gender, instagram_account, facebook_account, linkedin_account, image, lat, lon)
+VALUES (3, 'Bob', 'Smith', 'bobsmith@gmail.com', 'Sales Manager', 'MALE', 'bobsmith_insta', 'bobsmith_fb', 'bobsmith_linkedin', 'photo3.jpeg', 37.7749, -122.4194);
+
+INSERT INTO users (id, first_name, last_name, email, description, gender, instagram_account, facebook_account, linkedin_account, image, lat, lon)
+VALUES (4, 'Mary', 'Johnson', 'maryjohnson@gmail.com', 'Teacher', 'FEMALE', 'maryjohnson_insta', 'maryjohnson_fb', 'maryjohnson_linkedin', 'photo4.jpeg', 37.7749, -122.4194);
+
+INSERT INTO users (id, first_name, last_name, email, description, gender, instagram_account, facebook_account, linkedin_account, image, lat, lon)
+VALUES (5, 'David', 'Jones', 'davidjones@gmail.com', 'Accountant', 'MALE', 'davidjones_insta', 'davidjones_fb', 'davidjones_linkedin', 'photo5.jpeg', 37.7749, -122.4194);
+
+INSERT INTO users (id, first_name, last_name, email, description, gender, instagram_account, facebook_account, linkedin_account, image, lat, lon)
+VALUES (6, 'Samantha', 'Lee', 'samlee@gmail.com', 'Marketing Manager', 'FEMALE', 'samlee_insta', 'samlee_fb', 'samlee_linkedin', 'photo6.jpeg', 37.7749, -122.4194);
+
+INSERT INTO users (id, first_name, last_name, email, description, gender, instagram_account, facebook_account, linkedin_account, image, lat, lon)
+VALUES (7, 'Chris', 'Taylor', 'christaylor@gmail.com', 'Graphic Designer', 'MALE', 'christaylor_insta', 'christaylor_fb', 'christaylor_linkedin', 'photo7.jpeg', 37.7749, -122.4194);
+
+INSERT INTO users (id, first_name, last_name, email, description, gender, instagram_account, facebook_account, linkedin_account, image, lat, lon)
+VALUES (8, 'Karen', 'Clark', 'karenclark@gmail.com', 'HR Manager', 'FEMALE', 'karenclark_insta', 'karenclark_fb', 'karenclark_linkedin', 'photo8.jpeg', 37.7749, -122.4194);
