31d1fcafbc70cb5d4bd88d2fcce380d87685a2e0
tomegavazovv <tomegavazov@gmail.com>
2023-03-02 09:47:44 +0100

    s

--- src/main/java/com/example/stumble/StumbleApplication.java
+++ src/main/java/com/example/stumble/StumbleApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
-@PropertySource("classpath:application-${spring.active.profile:}.properties")
+@PropertySource("classpath:application-${spring.profiles.active:}.properties")
 public class StumbleApplication {
 
     public static void main(String[] args) {
